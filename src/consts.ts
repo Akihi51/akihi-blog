b4c47162f4571383ebe17e4c5dda20c65f3fb624
巷九凇 <120880023+Akihi51@users.noreply.github.com>
2026-02-11 14:22:43 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -12,5 +12,5 @@ export const GITHUB_CONFIG = {
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'akihi-blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2840334',
-	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
+	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'xiangjiusong',
 } as const
